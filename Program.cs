709957f373f8dac831cfe68b7f3d5c326b826135
myousefg <myousefg@gmail.com>
2024-03-13 15:05:21 +0700

    menambahkan implementasi dengan state-based construction

--- Program.cs
+++ Program.cs
@@ -13,11 +13,29 @@
     SAMOJA,
 }
 
+public enum DOORSTATES
+{
+    TERKUNCI,
+    TERBUKA,
+}
+
+public enum DOORTRIGGERS
+{
+    KunciPintu,
+    BukaPintu,
+}
+
 internal class Program
 {
     private static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        int kPos = KodePos.getKodePos(KELURAHAN.BATUNANGGAL);
+        Console.WriteLine("Kode pos Batununggal adalah: " + kPos);
+        DoorMachine door = new DoorMachine();
+        Console.WriteLine("Pintu saat ini sedang " + door.current);
+        door.Action(DOORTRIGGERS.BukaPintu);
+        door.Action(DOORTRIGGERS.KunciPintu);
+        door.Action(DOORTRIGGERS.KunciPintu);
     }
 }
 
@@ -27,5 +45,74 @@ public class KodePos
     {
         int[] kodePos = { 40266, 40287, 40267, 40256, 40287, 40286, 40286, 40286, 40272, 40274, 40273 };
         return kodePos[(int)kelurahan];
+    }
+}
+
+public class Shifts
+{
+    public DOORSTATES initDoorState;
+    public DOORSTATES finalDoorState;
+    public DOORTRIGGERS trigger;
+
+    public Shifts(DOORSTATES initDoorState, DOORSTATES finalDoorState, DOORTRIGGERS trigger)
+    {
+        this.initDoorState = initDoorState;
+        this.finalDoorState = finalDoorState;
+        this.trigger = trigger;
+    }
+
+}
+
+public class DoorMachine
+{
+    public DOORSTATES current;
+
+    public DoorMachine()
+    {
+        current = DOORSTATES.TERKUNCI;
+    }
+
+    Shifts[] Transitions =
+    {
+        new Shifts(DOORSTATES.TERBUKA, DOORSTATES.TERKUNCI, DOORTRIGGERS.KunciPintu),
+        new Shifts(DOORSTATES.TERKUNCI, DOORSTATES.TERBUKA, DOORTRIGGERS.BukaPintu),
+    };
+
+    public DOORSTATES NextState(DOORSTATES initState, DOORTRIGGERS trigger)
+    {
+        DOORSTATES finalState = initState;
+        for (int i = 0; i < Transitions.Length; i++)
+        {
+            Shifts change = Transitions[i];
+            if (initState == change.initDoorState && trigger == change.trigger)
+            {
+                finalState = change.finalDoorState;
+            }
+        }
+
+        return finalState;
+    }
+
+    public void Action(DOORTRIGGERS trigger)
+    {
+        current = NextState(current, trigger);
+
+        if (trigger == DOORTRIGGERS.KunciPintu)
+        {
+            Console.WriteLine("Mengunci pintu");
+        }
+        else if (trigger == DOORTRIGGERS.BukaPintu)
+        {
+            Console.WriteLine("Membuka kunci");
+        }
+
+        if (current == DOORSTATES.TERKUNCI)
+        {
+            Console.WriteLine("Pintu terkunci");
+        }
+        else if (current == DOORSTATES.TERBUKA)
+        {
+            Console.WriteLine("Pintu tidak terkunci");
+        }
     }
 }
