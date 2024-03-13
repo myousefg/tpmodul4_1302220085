637a15e8a18dbf9d29363b9eea64e2364111fbd5
myousefg <myousefg@gmail.com>
2024-03-13 14:02:31 +0700

    menambahkan implementasi dengan table-driven

--- Program.cs
+++ Program.cs
@@ -1,7 +1,31 @@
-﻿internal class Program
+﻿public enum KELURAHAN
+{
+    BATUNANGGAL,
+    KUJANGSARI,
+    MENGGER,
+    WATES,
+    CIJAURA,
+    JATISARI,
+    MARGASARI,
+    SEKEJATI,
+    KEBONWARU,
+    MALEER,
+    SAMOJA,
+}
+
+internal class Program
 {
     private static void Main(string[] args)
     {
         Console.WriteLine("Hello, World!");
+    }
+}
+
+public class KodePos
+{
+    public static int getKodePos(KELURAHAN kelurahan)
+    {
+        int[] kodePos = { 40266, 40287, 40267, 40256, 40287, 40286, 40286, 40286, 40272, 40274, 40273 };
+        return kodePos[(int)kelurahan];
     }
 }
